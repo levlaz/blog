63e43cff65d4d80d16aa6554ddafbac7fca24c6f
Lev Lazinskiy <lev@levlaz.org>
2017-01-21 22:22:44 -0800

    Release 1.0.1

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(
     name='blog',
     description='Simple single user blog written in Python 3 and Flask',
-    version='1.0',
+    version='1.0.1',
     author='Lev Lazinskiy',
     author_email='lev@levlaz.org',
     license='AGPLv3.0',
