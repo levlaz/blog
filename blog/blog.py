3263cf03e4b0954b27f8ca79fd151ad0186952ae
Lev Lazinskiy <lev@levlaz.org>
2017-01-21 23:42:13 -0800

    Refactor how tags are found

--- blog/blog.py
+++ blog/blog.py
@@ -78,9 +78,7 @@ def index():
 def show_post(post_slug):
     db = get_db()
     post = db.execute("SELECT * FROM posts WHERE slug = ?", [post_slug]).fetchone()
-    print(post)
-    tags = get_tags(post[0])
-    return render_template('post.html', post=post, tags=tags)
+    return render_template('post.html', post=post, get_tags=get_tags)
 
 @app.route('/edit/<int:id>', methods=['GET', 'POST'])
 def edit_post(id):
