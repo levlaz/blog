3397470cdd1f7a6b4c4e1bdd607336a281bfb3e9
Lev Lazinskiy <lev@levlaz.org>
2017-01-26 00:28:11 -0800

    Additional tests

---
+++ tests/unit_tests.py
@@ -0,0 +1,34 @@
+import os
+import sys
+import unittest
+import tempfile
+
+sys.path.insert(0, os.environ.get('BLOG_PATH'))
+from blog.blog import *
+
+class BlogUnitTestCase(unittest.TestCase):
+
+    def test_connect_db(self):
+        db = connect_db()
+        assert isinstance(db, sqlite3.Connection)
+
+    def test_get_db(self):
+        self.db, app.config['DATABASE'] = tempfile.mkstemp()
+        app.config['TESTING'] = True
+        self.app = app.test_client()
+        with app.app_context():
+            db = get_db()
+            assert isinstance(db, sqlite3.Connection)
+            
+            def schema():
+                return db.execute("SELECT * FROM sqlite_master").fetchall()
+
+            assert len(schema()) == 0
+
+            init = init_db()
+            
+            assert len(schema()) == 10
+
+if __name__ == '__main__':
+    unittest.main()
+
