2b8baaa351f7362e902e164df4ecfe9289833048
Lev Lazinskiy <lev@levlaz.org>
2017-06-28 23:56:05 -0700

    More DB tests

--- tests/unit_tests.py
+++ tests/unit_tests.py
@@ -29,6 +29,55 @@ class BlogUnitTestCase(unittest.TestCase):
 
             assert len(schema()) == 21
 
+    def test_get_static_pages(self):
+        assert len(get_static_pages()) == 0
+
+        self.db, app.config['DATABASE'] = tempfile.mkstemp()
+        app.config['TESTING'] = True
+        self.app = app.test_client()
+        with app.app_context():
+            migrate_db()
+            db = get_db()
+            cur = db.cursor()
+            cur.execute("""
+                INSERT INTO posts (title, slug, text_raw, text_compiled, is_static_page) \
+                VALUES("Static 1", "static-1", "", "", 0)
+                """)
+            cur.execute("""
+                INSERT INTO posts (title, slug, text_raw, text_compiled, is_static_page) \
+                VALUES("Static 2", "static-2", "", "", 0)
+                """)
+            cur.execute("""
+                INSERT INTO posts (title, slug, text_raw, text_compiled, is_static_page) \
+                VALUES("Static 3", "static-3", "", "", 0)
+                """)
+            db.commit()
+
+            assert len(get_static_pages()) == 3
+
+            cur.execute("""
+                INSERT INTO posts (title, slug, text_raw, text_compiled) \
+                VALUES("Post 1", "post-1", "", "")
+                """)
+            cur.execute("""
+                INSERT INTO posts (title, slug, text_raw, text_compiled) \
+                VALUES("Post 2", "post-2", "", "")
+                """)
+            cur.execute("""
+                INSERT INTO posts (title, slug, text_raw, text_compiled) \
+                VALUES("Post 3", "post-3", "", "")
+                """)
+            db.commit()
+
+            posts = cur.execute("SELECT * FROM posts WHERE is_static_page = 1").fetchall()
+
+            assert len(posts) == 3
+            assert len(get_static_pages()) == 3
+
+            posts_and_pages = cur.execute("SELECT * FROM posts").fetchall()
+            assert len(posts_and_pages) == 6
+
+
 if __name__ == '__main__':
     unittest.main()
 
