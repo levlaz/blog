ccab28439257779e98ae49b518faa80137fef9ab
Lev Lazinskiy <lev@levlaz.org>
2017-07-25 02:41:14 +0000

    Add functional test case

---
+++ tests/functional_tests.py
@@ -0,0 +1,33 @@
+import unittest
+import urllib2
+
+import os
+import sys
+
+from flask_testing import LiveServerTestCase
+from selenium import webdriver
+
+sys.path.insert(0, os.environ.get('BLOG_PATH'))
+from blog.blog import app
+
+class FunctionalTestCase(LiveServerTestCase):
+
+    def setUp(self):
+        self.db_fd, app.config['DATABASE'] = tempfile.mkstemp()
+        app.config['TESTING'] = True
+        with app.app_context():
+            cache.clear()
+            migrate_db()
+
+        self.driver = webdriver.Chrome()
+        self.driver.get(self.get_server_url())
+
+    def tearDown(self):
+        self.driver.quit()
+
+    def test_server_is_up_and_running(self):
+        res = urllib2.urlopen(self.get_server_url())
+        self.assertEqual(res.code, 200)
+
+if __name__ == '__main__':
+    unittest.main()
