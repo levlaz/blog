a22a60839db4c7bb8178fa23a9bc6c40123c0ad4
Lev Lazinskiy <lev@levlaz.org>
2017-11-24 11:37:23 -0800

    basic functional tests

--- tests/functional/functional_tests.py
+++ tests/functional/functional_tests.py
@@ -3,6 +3,7 @@ import sys
 import tempfile
 import unittest
 
+from pyvirtualdisplay import Display
 from selenium import webdriver
 from urllib.request import urlopen
 
@@ -12,17 +13,50 @@ from blog.blog import app
 class FunctionalTestCase(unittest.TestCase):
 
     def setUp(self):
-        self.driver = webdriver.Chrome()
         self.baseURL = "http://localhost:5000/"
 
+        if os.getenv('CIRCLECI'):
+            # Some additional stuff is requried in order to get this
+            # to work on CI. I am sure there is a cleaner way to
+            # do this, but this approach just works.
+            self.display = Display(visible=0, size=(1920, 1080)).start()
+            self.caps = webdriver.DesiredCapabilities().FIREFOX
+            self.caps["marionette"] = False
+            self.browser = webdriver.Firefox(capabilities=self.caps)
+            self.browser.maximize_window()
+            self.browser.implicitly_wait(10)
+        else:
+            self.browser = webdriver.Firefox()
+            self.browser.implicitly_wait(10)
+
     def tearDown(self):
-        self.driver.quit()
+        self.browser.quit()
 
-    def test_home(self):
-        self.driver.get(self.baseURL)
-        # FIXME get webdriver to work
-        assert True
+    def test_basics(self):
+        """Basic RealityCheck to make sure that the app loads and the
+        tests are working as expected."""
+        # when I go to the main website
+        self.browser.get(self.baseURL)
 
+        # I see that the page loads
+        self.assertIn('levlaz/blog', self.browser.page_source)
+
+    def test_admin(self):
+        # If I am an admin user
+        # when I go to the login page
+        self.browser.get(self.baseURL)
+        self.browser.find_element_by_xpath('/html/body/nav/a[5]').click()
+
+        # I see a Log In Form
+        self.assertIn('Log In', self.browser.page_source)
+
+        # When I submit an invalid password
+        self.browser.find_element_by_xpath('/html/body/form/input[1]').send_keys('invalid')
+        self.browser.find_element_by_xpath('/html/body/form/input[2]').click()
+
+        # I see an error message
+        error_message = self.browser.find_element_by_xpath('/html/body/p').text
+        self.assertEqual('Invalid password', error_message)
 
 if __name__ == '__main__':
     unittest.main()
